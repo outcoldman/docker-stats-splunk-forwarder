aeae1e23d56b9ccd3793199c9202a6403dd064ff
Denis Gladkikh <denis@gladkikh.email>
2015-08-22 17:46:57 -0700

    Remove date from top

--- docker_top.sh
+++ docker_top.sh
@@ -2,10 +2,9 @@
 
 DOCKER_BIN=$(dirname "$0")/docker
 for container_id in $("$DOCKER_BIN" ps -q); do
-  command_date=$(date -u +%Y-%m-%dT%H:%M:%S%z)
   "$DOCKER_BIN" top $container_id \
     -Ao pid,ppid,pgid,pcpu,vsz,nice,etime,time,tty,ruser,user,rgroup,group,comm,args:1000 | \
     tail -n +2 | \
     sed -E 's/  +/,/g' | \
-    awk "{print \"$command_date,$container_id,\"\$0}"
+    awk "{print \"$container_id,\"\$0}"
 done
