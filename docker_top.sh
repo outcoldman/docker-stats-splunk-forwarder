2f3ed23e49f39d762b0537ef941c6b0868b7598d
Denis Gladkikh <denis@gladkikh.email>
2015-08-22 18:06:29 -0700

    Add date back to docker top

--- docker_top.sh
+++ docker_top.sh
@@ -2,9 +2,10 @@
 
 DOCKER_BIN=$(dirname "$0")/docker
 for container_id in $("$DOCKER_BIN" ps -q); do
+  command_date=$(date -u +%Y-%m-%dT%H:%M:%S%z)
   "$DOCKER_BIN" top $container_id \
     -Ao pid,ppid,pgid,pcpu,vsz,nice,etime,time,tty,ruser,user,rgroup,group,comm,args:1000 | \
     tail -n +2 | \
     sed -E 's/  +/,/g' | \
-    awk "{print \"$container_id,\"\$0}"
+    sed -E "s/^/$command_date,$container_id,/"
 done
